6ad2f43aa4c487b16219b753f28b19da665642c4
Orlando <orlando.dan14@gmail.com>
2018-04-15 17:32:27 -0300

    Migrate theme

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,17 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require turbolinks
+//= require plugins/jquery-2.2.4.min
+//= require plugins/jquery.easing.min
+//= require bootstrap/js/bootstrap.min
+//= require plugins/OwlCarousel2-2.2.0/dist/owl.carousel.min
+//= require plugins/WOW/dist/wow.min
+//= require plugins/waypoints.min
+//= require plugins/Counter-Up-master/jquery.counterup.min
+//= require plugins/parallax.js-1.4.2/parallax.min
+//= require plugins/magnific-popup/dist/jquery.magnific-popup.min
+//= require plugins/jquery-validation-1.15.0/dist/jquery.validate.min
+//= require plugins/sscr
+//= require mail/contact
+//= require js/default
 //= require_tree .
